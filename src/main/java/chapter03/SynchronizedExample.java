83357dba2680edc29c3d71ba28ea301a4d3c010c
MasterTj <798750509@qq.com>
2018-12-22 20:29:59 +0800

    Java内存模型 .... 2018/12/22 20:29

--- src/main/java/chapter03/SynchronizedExample.java
+++ src/main/java/chapter03/SynchronizedExample.java
@@ -1,5 +1,9 @@
 package chapter03;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 class SynchronizedExample {
     int     a    = 0;
     boolean flag = false;
@@ -12,7 +16,34 @@ class SynchronizedExample {
     public synchronized void reader() { //ªÒ»°À¯
         if (flag) {
             int i = a;
+            System.out.println(i);
             //°≠°≠
         } // Õ∑≈À¯
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SynchronizedExample synchronizedExample=new SynchronizedExample();
+        ExecutorService executorService= Executors.newCachedThreadPool();
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+//                try {
+//                    Thread.sleep(20);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                synchronizedExample.writer();
+            }
+        });
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                synchronizedExample.reader();
+            }
+        });
+
+        executorService.shutdown();
+        executorService.awaitTermination(50, TimeUnit.SECONDS);
+        System.out.println("a: "+synchronizedExample.a+" flag:"+synchronizedExample.flag);
     }
 }
